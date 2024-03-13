82e21804794d35c77cec12c47ad749b11514a130
akshaygp18 <akshaygp41@gmail.com>
2024-03-13 12:37:02 +0530

    chatbot app.py file added

---
+++ app.py
@@ -0,0 +1,28 @@
+from langchain.llms import OpenAI
+from dotenv import load_dotenv
+load_dotenv()
+import streamlit as st
+import os
+
+def get_openai_response(question):
+    llm=OpenAI(model_name="gpt-3.5-turbo-1106",temperature=0.5)
+    response=llm(question)
+    return response
+
+
+##initialize our streamlit app
+
+st.set_page_config(page_title="Q&A Demo")
+
+st.header("Langchain Application")
+
+input=st.text_input("Input: ",key="input")
+response=get_openai_response(input)
+
+submit=st.button("Ask the question")
+
+## If ask button is clicked
+
+if submit:
+    st.subheader("The Response is")
+    st.write(response)
